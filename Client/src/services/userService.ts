1e317b3b13313d544c30fa1278262506c6c74632
s.viatkin <s.viatkin@loymax.ru>
2024-12-01 17:18:22 +0600

    * fix

--- Client/src/services/userService.ts
+++ Client/src/services/userService.ts
@@ -5,7 +5,9 @@ import {AxiosResponse} from "axios";
 const getUsers = async (): Promise<AxiosResponse<Promise<PagedResult<ShortUserModel>>>>  => await apiClient.get<PagedResult<ShortUserModel>>('/api/user/all', {
     withCredentials: true
 });
-const updateUser = async (command: UpdateUserModel) : Promise<AxiosResponse<true>>  => await apiClient.post(`/api/user/update`, command);
+const updateUser = async (command: UpdateUserModel) : Promise<AxiosResponse<true>>  => await apiClient.post(`/api/user/update`, command, {
+    withCredentials: true
+});
 const meUser = async (): Promise<AxiosResponse<string>> => await apiClient.get('/api/user/me', { 
     withCredentials: true
 });
